f9b0f6112c6ed372a01c70ec4153d937fc80289b
Tom Vandorpe <tom.vandorpe@dataroots.io>
2024-12-18 13:13:11 +0000

    changes

--- models/staging/stg_customers.sql
+++ models/staging/stg_customers.sql
@@ -1,3 +1,10 @@
-select *
 
-from {{ source('jaffle_shop', 'orders') }}
+{{
+    config(
+        materialized='table'
+    )
+}}
+
+select ID as customer_ID, FIRST_NAME, LAST_NAME
+
+from {{ source('jaffle_shop', 'customers') }}
