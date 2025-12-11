3659c9993c63235289e844a34bed02433291c13e
Tom Vandorpe <tom.vandorpe@dataroots.io>
2025-12-11 22:04:56 +0000

    update

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -1,23 +1,12 @@
 with customers as (
 
-    select
-        id as customer_id,
-        first_name,
-        last_name
-
-    from raw.jaffle_shop.customers
+    select * from {{ ref('stg_jaffle_shop__customers') }}
 
 ),
 
 orders as (
 
-    select
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status
-
-    from raw.jaffle_shop.orders
+    select * from {{ ref('stg_jaffle_shop__orders') }}
 
 ),
 
@@ -26,8 +15,8 @@ customer_orders as (
     select
         customer_id,
 
-        min(order_date) as first_order_date,
-        max(order_date) as most_recent_order_date,
+        min(order_placed_at) as first_order_date,
+        max(order_placed_at) as most_recent_order_date,
         count(order_id) as number_of_orders
 
     from orders
--- models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
@@ -1,14 +1,8 @@
-{{config(materialized='view') }}
 
-
-with
-    orders_base as (
         select
             id as order_id,
             user_id as customer_id,
             order_date as order_placed_at,
             status as order_status
 
-        from {{ source("jaffle_shop", "orders") }})
-
-        select * from orders_base
+        from {{ source("jaffle_shop", "orders") }}
