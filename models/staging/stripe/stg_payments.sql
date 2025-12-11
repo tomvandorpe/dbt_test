21760c17a2358d29d1b92406079ae59facced21a
Tom Vandorpe <tom.vandorpe@dataroots.io>
2025-12-11 20:58:31 +0000

    update

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -1,24 +1,10 @@
-with payments_base as (
+with payments as (
         select
         ID as transaction_seq,
         *
         from {{ source("stripe", "payment") }}
     ),
 
+SELECT * from payments
 
-    orders_base as (
-        select * from {{ ref('stg_orders') }}
-    ),
 
-    orders_payments as (
-
-        select payments.*,
-               orders.order_status
-
-        from payments_base as payments
-
-    left join orders_base as orders
-    on payments.orderid = orders.id
-    )
-
-select * from orders_payments
